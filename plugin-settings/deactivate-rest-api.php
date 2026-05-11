72c7110e20ffb0efae554d50ffa3442d0599d979
David Arnado <77515359+darnado@users.noreply.github.com>
2026-05-11 12:57:53 -0500

    fix: add fallback to the deactivate rest api feature
    Adds a fallback return value to the function in charge of deactivating the REST API to unauthenticated requests.
    It also adds missing comments in phpdocs format.

--- plugin-settings/deactivate-rest-api.php
+++ plugin-settings/deactivate-rest-api.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * Restricts access to the WordPress REST API.
+ *
+ * @package Caledros_Helper
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 /**
@@ -24,23 +29,39 @@ if ( ! defined( 'ABSPATH' ) ) {
  * with Caledros Helper; if not, see <https://www.gnu.org/licenses/>.
  */
 
-// Deactivate REST API for non-authenticated users
+/**
+ * Restrict access to the WordPress REST API
+ *
+ * When the 'caledros_helper_deactivate_rest_api' option is equal to 1, then
+ * unauthenticated requests are rejected with a 401 WP_Error. When the requests
+ * are already authenticated, or rejected, by prior callbacks, then the
+ * authentication result is passed through unchanged.
+ *
+ * Hooked into the 'rest_authentication_errors' filter.
+ *
+ * @param null|true|WP_Error $result    Authentication result from prior callbacks.
+ * @return null|true|WP_Error           401 WP_Error or unchanged authentication result
+ *                                      from previous callbacks.
+ */
 add_filter(
 	'rest_authentication_errors',
 	function ( $result ) {
-		if ( get_option( 'caledros_helper_deactivate_rest_api', 1 ) ) {
-			if ( true === $result || is_wp_error( $result ) ) {
-				return $result;
-			}
-
-			if ( ! is_user_logged_in() ) {
-				return new WP_Error(
-					'rest_not_logged_in',
-					__( 'This is not the page you are looking for', 'caledros-helper' ),
-					array( 'status' => 401 )
-				);
-			}
+		if ( ! get_option( 'caledros_helper_deactivate_rest_api', 1 ) ) {
 			return $result;
 		}
+
+		if ( true === $result || is_wp_error( $result ) ) {
+			return $result;
+		}
+
+		if ( ! is_user_logged_in() ) {
+			return new WP_Error(
+				'rest_not_logged_in',
+				__( 'This is not the page you are looking for', 'caledros-helper' ),
+				array( 'status' => 401 )
+			);
+		}
+
+		return $result;
 	}
 );
